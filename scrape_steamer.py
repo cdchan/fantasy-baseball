57b501507d40802d7f20ab0d46360a533ca4935b
Chris Chan <cdchan@gmail.com>
2018-11-13 11:57:56 -0500

    update scraping projections

--- scrape_steamer.py
+++ scrape_steamer.py
@@ -17,41 +17,44 @@ def main():
     args = parser.parse_args()
 
     if args.draft:
-        projection_type = "fangraphsdc"
+        projection_types = ["fangraphsdc"]
     else:
-        projection_type = "rfangraphsdc"  # rest of season projections
+        projection_types = []
+        projection_types.append("rfangraphsdc")  # rest of season projections
+        projection_types.append("steamer600u")
 
-    # save projections for current and historical usage
-    filenames = [
-        'historical/depthcharts_{}_{:%Y%m%d}.csv',
-        'depthcharts_{}.csv'
-    ]
+    for projection_type in projection_types:
+        # save projections for current and historical usage
+        filenames = [
+            'historical/{}_{}_{:%Y%m%d}.csv',
+            '{}_{}.csv'
+        ]
 
-    params = {
-        'pos': 'all',
-        'type': projection_type,
-    }
+        params = {
+            'pos': 'all',
+            'type': projection_type,
+        }
 
-    player_types = [
-        {'type': 'batters', 'stats': 'bat'},
-        {'type': 'pitchers', 'stats': 'pit'}
-    ]
+        player_types = [
+            {'type': 'batters', 'stats': 'bat'},
+            {'type': 'pitchers', 'stats': 'pit'}
+        ]
 
-    for player_type in player_types:
-        params['stats'] = player_type['stats']
+        for player_type in player_types:
+            params['stats'] = player_type['stats']
 
-        r = requests.post("http://www.fangraphs.com/projections.aspx", params=params, data=fangraphs_form_data)
+            r = requests.post("https://www.fangraphs.com/projections.aspx", params=params, data=fangraphs_form_data)
 
-        write_csvs(r, player_type['type'], filenames)
+            write_csvs(r, projection_type, player_type['type'], filenames)
 
 
-def write_csvs(r, player_type, filenames):
+def write_csvs(r, projection_type, player_type, filenames):
     """
     Write the response to CSVs with `filenames`
 
     """
     for filename in filenames:
-        with open(filename.format(player_type, datetime.datetime.today()), 'w') as output_file:
+        with open(filename.format(projection_type, player_type, datetime.datetime.today()), 'w') as output_file:
             output_file.write(r.text[1:].encode('utf8'))  # remove the first 3 characters which are BOM
 
 
